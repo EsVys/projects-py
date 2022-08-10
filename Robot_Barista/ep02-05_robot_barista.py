5f4768f56cb2246c1d35cd33af9326aaab5db396
EsVys <108893515+EsVys@users.noreply.github.com>
2022-08-10 14:38:32 +0200

    Update ep02-05_robot_barista.py
    Add price calculation

--- Robot_Barista/ep02-05_robot_barista.py
+++ Robot_Barista/ep02-05_robot_barista.py
@@ -20,7 +20,9 @@ items_with_milk = {
 }
 items_with_milk_str = str(items_with_milk)
 
-def greet(greeting):
+
+
+def greet(name, greeting):
     print('Hello! Welcome to our coffe shop.')
 
     name = input('What is your name?\n')
@@ -29,13 +31,77 @@ def greet(greeting):
     if not bool(name):
         name = 'Darling'
 
-    age = int(input('What is your age, ' + str(proper_name[name]) + '?\n'))
+    greeting = 'Hi!'
+    # name = greet(greeting)
+
+    return(name, greeting)
+
+def age_validation(age):
+    age = int(input('What is your age, ' + proper_names[name] + '?\n'))
+    validate_input()
     if age <= 16:
         print('We are sorry, ' + proper_name[name] + ' we do not anything for you here.')
         exit()
-        
-    return name
 
+    return(age)
+
+def validate_input():
+    age = input('What is your age?\n')
+    while age is not int:
+        print('The value is not correct, only numbers are allowed.')
+        if age is int:
+            return(age)
+        break
+
+    validate_input()
+
+def evil_status():
+    name = greet(greeting)
+    if (name == 'ben') or (name == 'pat'):
+        evilst = input('Are you evil?\n').lower()
+        if evilst == 'no':
+            print('Oh, come on in!')
+        elif evilst == 'yes' and 4 <= int(input('How many good deeds have you done today?\n')):
+            print('All right, you can have a coffee.')
+        else: 
+            print('No coffee for you!.')
+            exit()
+
+def get_order(name: str):
+    #menu, order
+    print('Here is our menu. \n' + items_without_milk_str + ', ' + items_with_milk_str + '.')
+    order = input('What would you like?\n').lower()
+
+    #price calculation
+    if order in items_without_milk:
+        price = items_without_milk[order]
+    elif order in items_with_milk:
+        price = items_with_milk[order]
+    else:
+        print('Sorry, we do not have that here.')
+        exit()
+    quantity = int(input('How many coffees would you like?\n'))
+    total = float(price) * float(quantity)
+    print('The price is $' + str(total) + '.')
+
+
+    #total price calculation, purchase completion
+    #total = calculate_total(price, quantity)
+
+    final_print(order, quantity)
+
+    #when the customer returns, the system remembers them
+
+    repeat_order(order, name, total)
+
+#def calculate_total(price: float, quantity: int) -> float:
+    #total = (quantity * price)
+    #print_total(total)
+    #return total
+
+#def print_total(total):
+    #print('That will be $' + str(total) + '.')
+        
 def final_print(order, quantity):
     we_will = 'We will have your '
     ready = ' ready in a minute.'
@@ -51,25 +117,6 @@ def final_print(order, quantity):
     
     return order
 
-def evil_status(name):
-    if (name == 'ben') or (name == 'pat'):
-        evilst = input('Are you evil?\n').lower()
-        if evilst == 'no':
-            print('Oh, come on in!')
-        elif evilst == 'yes' and 4 <= int(input('How many good deeds have you done today?\n')):
-            print('All right, you can have a coffee.')
-        else: 
-            print('No coffee for you!.')
-            exit()
-
-def calculate_total(price: float, quantity: int) -> float:
-    total = (quantity * price)
-    print_total(total)
-    return total
-
-def print_total(total):
-    print('That will be $' + str(total) + '.')
-
 def repeat_order(order, name, total):
     print('Hello, welcome back ' + proper_name[name] + '!')
     repeat = input('Should I repeat your last order?\n').lower()
@@ -81,50 +128,19 @@ def repeat_order(order, name, total):
         get_order(name)
 
 def main():
-    #greeting, age check
     greeting = 'Hi!'
     name = greet(greeting)
-
-    #name, evil status check   
-    evil_status(name)
-    get_order(name)
-
-def get_order(name: str):
-    #menu, order
-    print('Here is our menu. \n' + items_without_milk_str + ', ' + items_with_milk_str + '.')
-    order = input('What would you like?\n').lower()
-
-    #price calculation
-    if order == 'lungo':
-        price = 3.50
-    elif order == 'dopio':
-        price = 4
-    elif order == 'latte':
-        price = 5
-    elif order == 'espresso':
-        milk = input('With oat milk? Or without?\n').lower()
-        if milk == 'with':
-            price = 3.25
-        elif milk == 'without':
-            price = 3
-        else:
-            print('Please chose valid option.')
-            input().lower()
-    else:
-        print('Sorry, we do not have that here.')
-        price = 0
-        exit()
-
-    quantity = int(input('How many coffees would you like?\n'))
-
-    #total price calculation, purchase completion
-    total = calculate_total(price, quantity)
-
+    greet(greeting)
+    age_validation(age)
+    evil_status()
+    get_order(name: str)
+    calculate_total(price: float, quantity: int) -> float:
+    print_total(total)
     final_print(order, quantity)
-
-    #when the customer returns, the system remembers them
-
     repeat_order(order, name, total)
+    
+
+
 
 if __name__ == "__main__":
     main()
