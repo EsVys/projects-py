f56b88a93f5b093df013d124f938410600ea0a45
EsVys <108893515+EsVys@users.noreply.github.com>
2022-08-11 22:24:43 +0200

    Update ep02-05_robot_barista.py

--- Robot_Barista/ep02-05_robot_barista.py
+++ Robot_Barista/ep02-05_robot_barista.py
@@ -1,8 +1,8 @@
 # To Do:
-# finish def get_order
-# fix when no name print 'Darling'
+# finish main function
 # fix input name and proper_name
-# solution for milk input (dictionary)
+# input validation - quantity, milk, (age done)
+# write my own file with functions
 # with the next order the robot will ask if the customer wants to repeat the next order using the variables from functions
 # if the customer does not wish to repeat order start with new order
 
@@ -20,8 +20,15 @@ items_with_milk = {
 }
 items_with_milk_str = str(items_with_milk)
 
+<<<<<<< Updated upstream
 def greet(greeting):
     print('Hello! Welcome to our coffe shop.')
+=======
+
+
+def greet(greeting: str):
+    print(greeting + ' Welcome to our coffe shop.')
+>>>>>>> Stashed changes
 
     name = input('What is your name?\n')
     proper_name = {}
@@ -29,10 +36,89 @@ def greet(greeting):
     if not bool(name):
         name = 'Darling'
 
+<<<<<<< Updated upstream
     age = int(input('What is your age, ' + str(proper_name[name]) + '?\n'))
+=======
+    return name
+
+def age_validation(age,proper_name):
+    age = int(input('What is your age, ' + proper_name + '?\n'))
+    validate_input()
+>>>>>>> Stashed changes
     if age <= 16:
-        print('We are sorry, ' + proper_name[name] + ' we do not anything for you here.')
+        print('We are sorry, ' + proper_name + ' we do not anything for you here.')
         exit()
+<<<<<<< Updated upstream
+=======
+
+    return age
+
+def validate_input():
+    age = input('What is your age?\n')
+    try:
+        age = int(age)
+        # use age_validation
+        if age <= 0:
+            print('The value is not correct, only positive numbers are allowed.')
+            return validate_input()
+        return age
+    except ValueError:
+        print('The value is not correct, only numbers are allowed.')
+        return validate_input()
+
+def evil_status(name):
+    if (name == 'ben') or (name == 'pat'):
+        evilst = input('Are you evil?\n').lower()
+        if evilst == 'no':
+            print('Oh, come on in!')
+        elif evilst == 'yes' and 4 <= int(input('How many good deeds have you done today?\n')):
+            print('All right, you can have a coffee.')
+        else: 
+            print('No coffee for you!.')
+            exit()
+
+def add_milk(price_without_milk: int):
+    milk = input('Do you wish to add oat milk?\n').lower()
+    if milk == 'yes': 
+        return price_without_milk + 0.25
+    elif milk == 'no':
+        return price_without_milk
+
+def get_order(name: str):
+    #menu, order
+    print('Here is our menu. \n' + items_without_milk_str + ', ' + items_with_milk_str + '.')
+    order = input('What would you like?\n').lower()
+
+    #price calculation
+    if order in items_without_milk:
+        price = add_milk(items_without_milk[order])
+    elif order in items_with_milk:
+        price = items_with_milk[order]
+    else:
+        print('Sorry, we do not have that here.')
+        exit()
+    quantity = int(input('How many coffees would you like?\n'))
+    total = float(price) * float(quantity)
+    print('The price is $' + str(total) + '.')
+
+
+    #total price calculation, purchase completion
+    #total = calculate_total(price, quantity)
+
+    final_print(order, quantity)
+
+    #when the customer returns, the system remembers them
+
+    #repeat_order(order, name, total)
+
+#def calculate_total(price: float, quantity: int) -> float:
+    #total = (quantity * price)
+    #print_total(total)
+    #return total
+
+#def print_total(total):
+    #print('That will be $' + str(total) + '.')
+>>>>>>> Stashed changes
         
     return name
 
@@ -40,14 +126,14 @@ def final_print(order, quantity):
     we_will = 'We will have your '
     ready = ' ready in a minute.'
     hot_warning = 'Be careful, this drink is hot.'
-    if order in items_without_milk[0:3] and quantity == 1:
-        print(we_will + order + ready + '\n' + hot_warning)
-    elif order in items_without_milk[0:3] and quantity >= 2:
-        print(we_will + str(quantity) + " " + order + 's' + ready + '\n' + hot_warning)
-    elif order == items_without_milk[3] and quantity == 1:
-        print(we_will + str(quantity) + " " + order + ready)
-    elif order == items_without_milk[3] and quantity >= 2:
-        print (we_will + str(quantity) + " " + order + 's' + ready)
+    #if order in items_without_milk[0:3] and quantity == 1:
+        #print(we_will + order + ready + '\n' + hot_warning)
+    #elif order in items_without_milk[0:3] and quantity >= 2:
+        #print(we_will + str(quantity) + " " + order + 's' + ready + '\n' + hot_warning)
+    #elif order == items_without_milk[3] and quantity == 1:
+        #print(we_will + str(quantity) + " " + order + ready)
+    # elif order == items_without_milk[3] and quantity >= 2:
+        #print (we_will + str(quantity) + " " + order + 's' + ready)
     
     return order
 
@@ -71,7 +157,7 @@ def print_total(total):
     print('That will be $' + str(total) + '.')
 
 def repeat_order(order, name, total):
-    print('Hello, welcome back ' + proper_name[name] + '!')
+    print('Hello, welcome back ' + proper_name + '!')
     repeat = input('Should I repeat your last order?\n').lower()
     if repeat == 'yes':
         confirmation = input('Your last order was: ' + order + '. Is it correct?').lower()
@@ -81,6 +167,7 @@ def repeat_order(order, name, total):
         get_order(name)
 
 def main():
+<<<<<<< Updated upstream
     #greeting, age check
     greeting = 'Hi!'
     name = greet(greeting)
@@ -121,6 +208,13 @@ def get_order(name: str):
     total = calculate_total(price, quantity)
 
     final_print(order, quantity)
+=======
+    greeting = 'Hello!'
+    name = greet(greeting)
+    age = validate_input()
+    evil_status(name)
+    get_order(name)
+>>>>>>> Stashed changes
 
     #when the customer returns, the system remembers them
 
